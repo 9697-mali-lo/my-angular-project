64e34a9cb575910727f00fdbce706647a32333dd
Mali Lorents <6719697@gmail.com>
2026-01-30 09:09:49 +0200

    date

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { Register } from './components/Auth/register/register';
 import { TeamList } from './components/Teams/team-list/team-list';
 import { ProjectBoard } from './components/Projects/project-board/project-board';
 import { TaskBoard } from './components/Task/task-board/task-board';
+import { TaskCommentsPage } from './components/comments/task-comments-page/task-comments-page';
 //  import {TaskBoard}from 'C:\Users\user1\Desktop\angular-project\my-angular-app\src\app\components\Task\task-board'  
 export const routes: Routes = [
     { path: '', redirectTo: 'auth/login', pathMatch: 'full' },
@@ -22,7 +23,7 @@ export const routes: Routes = [
   { path: 'projects/:teamId',component:ProjectBoard },
   // { path: 'projects/:teamId', component: ProjectsComponent },
   { path: 'tasks',component:TaskBoard },
-  { path: 'tasks/:taskId/comments', component: TaskCommentsComponent },
+  { path: 'tasks/:taskId/comments', component: TaskCommentsPage },
   { path: 'projects/:projectId/tasks', component: TaskBoard }
 
    
